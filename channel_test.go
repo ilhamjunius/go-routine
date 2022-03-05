bfc0821db47397d4cd5da3855168e09b29c4c129
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-05 16:22:41 +0700

    In Out channel function

--- channel_test.go
+++ channel_test.go
@@ -32,3 +32,21 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 	defer close(channel)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ilham Junius"
+}
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}

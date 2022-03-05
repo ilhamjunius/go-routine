cd930e66336e588f190891381f9888ca343ce3d0
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-05 18:44:49 +0700

    RW Mutex

--- mutex_test.go
+++ mutex_test.go
@@ -22,3 +22,37 @@ func TestMutex(t *testing.T) {
 	time.Sleep(5 * time.Second)
 	fmt.Println("Counter = ", x)
 }
+
+type BankAccount struct {
+	RWmutex sync.RWMutex
+	Balance int
+}
+
+func (account *BankAccount) AddBalance(amount int) {
+	account.RWmutex.Lock()
+	account.Balance = account.Balance + amount
+	account.RWmutex.Unlock()
+}
+
+func (account *BankAccount) GetBalance() int {
+	account.RWmutex.RLock()
+	balance := account.Balance
+	account.RWmutex.RUnlock()
+	return balance
+}
+
+func TestRWMutex(t *testing.T) {
+	account := BankAccount{}
+
+	for i := 0; i < 100; i++ {
+		go func() {
+			for j := 0; j < 100; j++ {
+				account.AddBalance(1)
+				fmt.Println(account.GetBalance())
+			}
+		}()
+	}
+
+	time.Sleep(5 * time.Second)
+	fmt.Println("Total Balance : ", account.Balance)
+}

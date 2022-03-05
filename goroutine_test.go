98e0c68f79c4b33e6bd6301b9e755770086c09a9
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-05 14:07:45 +0700

    create test function

--- goroutine_test.go
+++ goroutine_test.go
@@ -1,7 +1,17 @@
 package goroutine
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+	"time"
+)
 
 func RunHelloWorld() {
 	fmt.Println("Hello World")
+}
+
+func TestCreateGoroutine(t *testing.T) {
+	go RunHelloWorld()
+	fmt.Println("Ups")
+	time.Sleep(1 * time.Second)
 }

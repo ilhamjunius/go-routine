9add3eeceb34485f8bec442a45d97d16965f61be
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-05 17:45:38 +0700

    default select channel

--- channel_test.go
+++ channel_test.go
@@ -109,3 +109,29 @@ func TestSelectChannel(t *testing.T) {
 		}
 	}
 }
+
+func TestDefaultSelectChannel(t *testing.T) {
+	counter := 0
+	channel1 := make(chan string)
+	channel2 := make(chan string)
+	defer close(channel1)
+	defer close(channel2)
+
+	go GiveMeResponse(channel1)
+	go GiveMeResponse(channel2)
+	for {
+		select {
+		case data := <-channel1:
+			fmt.Println("Data dari channel 1", data)
+			counter++
+		case data := <-channel2:
+			fmt.Println("Data dari channel 2", data)
+			counter++
+		default:
+			fmt.Println("Menunggu data")
+		}
+		if counter == 2 {
+			break
+		}
+	}
+}

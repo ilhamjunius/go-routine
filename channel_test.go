5d756cdde122ab9a28e116f33cc621f0bbbda829
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-05 15:33:49 +0700

    channel as parameter

--- channel_test.go
+++ channel_test.go
@@ -19,3 +19,16 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Ilham Junius"
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	go GiveMeResponse(channel)
+	data := <-channel
+	fmt.Println(data)
+	defer close(channel)
+}

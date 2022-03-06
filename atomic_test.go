4033d0373d9d9ac595938f3ee2568c7c85ac7457
Ilham Junius <ilhamjunius712@gmail.com>
2022-03-06 12:55:11 +0700

    sync atomic

--- atomic_test.go
+++ atomic_test.go
@@ -2,19 +2,28 @@ package goroutine
 
 import (
 	"fmt"
+	"sync"
+	"sync/atomic"
 	"testing"
-	"time"
 )
 
-func TestRaceCondition(t *testing.T) {
-	x := 0
+func TestAtomic(t *testing.T) {
+	var x int64 = 0
+	group = sync.WaitGroup{}
+
 	for i := 0; i < 1000; i++ {
+
 		go func() {
+			group.Add(1)
+
 			for j := 0; j < 100; j++ {
 				x = x + 1
+				atomic.AddInt64(&x, 1)
 			}
+			group.Done()
 		}()
 	}
-	time.Sleep(5 * time.Second)
+
+	group.Wait()
 	fmt.Println("Counter = ", x)
 }
